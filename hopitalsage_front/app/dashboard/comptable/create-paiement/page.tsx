5365a27bb9c2faba8a4cc7a4784db960dbbd86ad
winica <winicaloseka@gmail.com>
2025-05-26 22:24:57 +0100

    Jesus C,

---
+++ hopitalsage_front/app/dashboard/comptable/create-paiement/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function CreatePaiementPage() {
+  return (
+    <div>
+      <h1>Créer un Paiement</h1>
+      {/* Ici, tu peux afficher ton modal ou un bouton pour l’ouvrir */}
+    </div>
+  );
+}
