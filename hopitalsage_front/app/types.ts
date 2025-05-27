72c943d10c86e726582f352837c2c22ce15355f0
winica <winicaloseka@gmail.com>
2025-05-27 12:01:02 +0100

    jornica kayembe nkola

---
+++ hopitalsage_front/app/types.ts
@@ -0,0 +1,11 @@
+// src/types.ts (ou app/types.ts selon ta structure)
+
+export interface Eleve {
+  id: number;
+  nom_elev: string;
+  postnom_elev: string;
+  prenom_elev: string;
+  matricule_elev: string;
+  option_elev: string;
+  classe_elev: string;
+}
